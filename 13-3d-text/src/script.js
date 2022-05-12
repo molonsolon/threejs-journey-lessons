1514a07685ea007ef7ffea4d63db9d5c0114ad9b
Joseph Coulter <joeycoulter@gmail.com>
2022-05-12 13:33:14 -0700

    Lesson 13 complete

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -3,8 +3,8 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
 import typefacefont from "three/examples/fonts/helvetiker_regular.typeface.json";
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 
 /**
  * Base
@@ -18,36 +18,65 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// Axes Helper
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load("/textures/matcaps/8.png");
 
 /**
  * Fonts
  */
 const fontLoader = new FontLoader();
 
-fontLoader.load(
-    '/fonts/CirrusCumulus_Regular.json',
-    (font) => {
-        const textGeometry = new TextGeometry('Hello Three.js!', {
-            font: font,
-            size: 0.5,
-            height: 0.2,
-            curveSegments: 12,
-            bevelEnabled: true,
-            bevelThickness: 0.03,
-            bevelSize: 0.02,
-            bevelOffset: 0,
-            bevelSegments: 5
-        })
-        const textMaterial = new THREE.MeshBasicMaterial()
-        textMaterial.wireframe = true;
-        const text = new THREE.Mesh(textGeometry, textMaterial)
-        scene.add(text)
-    }
-)
+fontLoader.load("/fonts/CirrusCumulus_Regular.json", (font) => {
+  const textGeometry = new TextGeometry("Hello Three.js!", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 6,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  // textGeometry.computeBoundingBox()
+  // textGeometry.translate(
+  //     -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+  //     -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
+  //     -(textGeometry.boundingBox.max.z - 0.03) * 0.5,
+  // )
+  textGeometry.center();
+  const material = new THREE.MeshMatcapMaterial();
+  material.matcap = matcapTexture;
+  
+  const text = new THREE.Mesh(textGeometry, material);
+  scene.add(text);
+
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+ 
+
+  for (let i = 0; i < 100; i++) {
+
+    const donut = new THREE.Mesh(donutGeometry, material);
+
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+
+    const scale = Math.random()
+    donut.scale.set(scale, scale, scale)
+    scene.add(donut);
+  }
+});
 
 /**
  * Sizes
--- 13-3d-text/static/notes.md
+++ 13-3d-text/static/notes.md
@@ -13,3 +13,29 @@ We are going to use `TextBufferGeometry`. Do not use example in docs because val
 
 Creating text geometry is long and hard for the computer. Avoid doing it many times and keep the geometry as low poly as possible by reducing the `curveSegments` and `bevelSegments` Remove the wireframe once happy with the level of detail.
 
+## Center the text
+There are multiple options for centering the text
+
+### Using the bounding
+The bounding is info associated with the geometry that tells what space is taken by that geometry. It can be a box or a sphere.
+
+Bounding helps three.js calculate if the object is on the screen (frustum culling).
+It can be used to recenter the geometry. Frustum culling is useful to avoid computation of things that are not visible. By default, Three.js uses sphere bounding. calc the box bounding with `computeBoundingBox()`. The result is an instance of Box3 with `min` and `max` props. the `min` isn't at `0` because of the `bevelThickness` and `bevelSize`. 
+
+Instead of moving the mesh, we are going to move the whole geometry with `translate(...)` You multiply each `max` value of the bounding box by `0.5` in order to move it over by exactly half the bounding box. This text looks centered but it's not because of the `bevelThickness` and `bevelSize`. To solve, subtract the value of `bevelSize` from the x and y and `bevelThickness` from the z.
+
+Well... the above method is the long, manual way. To do this simply, just use the `center()` method on the geometry.
+
+## Add a matcap material
+Use the given matcaps in `/static/textures/matcaps/` or you can download one from:
+https://github.com/nidorx/matcaps 
+
+Load the texture with `TextureLoader`, use `MeshMatcapMaterial` and apply the loaded `matcapTexture` variable with the `matcap` prop. 
+
+## Add Objects
+Create 100 donuts within the success function of the texture loader. We can add randomness in the position of these donuts before adding to the scene by modifying the `position` coordinates and `Math.random()`. Do the same for two axes on the rotation. Then, create a variable with  single random value and apply that to all axes for the `scale` prop.
+
+## Optimize
+Our code nees optimization because of the slow loading time. We can use the same material and same geometry on multiple meshes. Move the geometry and material of the donuts outside of the loop function.
+
+Go further by using the same material as the `text`. 
